7a71f4d61936dd4ebf1dcc7a0fae4c3bf8d29ec9
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:53:13 +0200

    Arreglado fallos

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        //Con el scanner coger un numero entero positivo y una cadena.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un numero positivo: ");
+        int numero = scanner.nextInt();
+        System.out.println("Introduce una cadena de texto: ");
+        String cadena = scanner.next();
+        System.out.println("Número: " + numero + " || Texto: " + cadena);
 
-        //Ver si el string es palindromo.
+        // ver cuantas mayusculas tiene la cadena.
+        int cantidadMayusculas= 0;
+        for (int i = 0; i < cadena.length(); ++i){
+            if (Character.isUpperCase(cadena.charAt(i))){
+                cantidadMayusculas++;
+            }
+        }
+        System.out.println("La cadena de texto "+cadena+" tiene un total de "+cantidadMayusculas+" mayusculas.");
+
+        // Ver si el string es palindromo.
         System.out.println("Escribe un texto para ver si es palíndromo");
         System.out.print("> ");
-        String textPal = sc.next();
-        sc.nextLine();
+        String textPal = scanner.next();
+        scanner.nextLine();
         String textReverse = "";
         for(int i = textPal.length() - 1; i >= 0; i--){
             textReverse += textPal.charAt(i);
@@ -18,12 +31,10 @@ public class Main {
         String resultText = textPal.equals(textReverse) ? "Es palíndromo" : "No es palíndromo";
         System.out.println(resultText);
 
-        //ver cuantas mayusculas tiene la cadena.
-
-        //Si no es primo, factorizarlo.
+        // Si no es primo, factorizarlo.
         System.out.println("Escribe un número para saber si es primo. Si no lo es se factoriza");
         System.out.print("> ");
-        int numPrimo = sc.nextInt();
+        int numPrimo = scanner.nextInt();
         boolean isPrimo = true;
         for(int i = 2; i <= Math.round(numPrimo / 2); i++){
             if(numPrimo % i == 0) isPrimo = false;
@@ -40,6 +51,12 @@ public class Main {
             System.out.println(textFactoriales);
         }
 
-        //Mostrar el tamaño de la cadena.
+        // Mostrar el tamaño de la cadena.
+        System.out.println("Méteme una cadena: ");
+        scanner.useDelimiter("\n");
+        String textToLength = scanner.nextLine();
+        System.out.println("La longitud es: " + textToLength.length());
+        scanner.close();
     }
+    
 }
