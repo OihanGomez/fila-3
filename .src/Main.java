821787843a7678f877b35c601ed0df3daefdcfa5
JaimeManero <jmaneromo23de@ikzubirimanteo.com>
2023-10-23 12:38:24 +0200

    Codigo principal.

---
+++ .src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+//Con el scanner coger un numero entero positivo y una cadena.
+
+//Ver si el string es palindromo.
+
+//ver cuantas mayusculas tiene la cadena.
+
+//Si no es primo, factorizarlo.
+
+//Mostrar el tamaño de la cadena.
+    }
+}
