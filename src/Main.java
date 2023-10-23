dc7348e612437a93b6cacc4565901132281232e0
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:31:23 +0200

    mostrar tamaño de cadena

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Meteme una cadena: ");
+        String cadena = scanner.next();
+        scanner.nextLine();
+        System.out.println("La longitud es: " + cadena.length());
 
 //Con el scanner coger un numero entero positivo y una cadena.
 
