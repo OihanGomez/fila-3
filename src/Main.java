1a0ef0fa9c9b8d66d4a5f0e0acfcf1f26e4c188a
DW31 TALDEA <ik012982i9@H012982.NET>
2023-10-23 13:25:21 +0200

    Crear Scanner con numero entero y cadena, y ver cuantas mayusculas tiene la cadena.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ public class Main {
     public static void main(String[] args){
 
 //Con el scanner coger un numero entero positivo y una cadena.
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un numero positivo: ");
+        int numero = scanner.nextInt();
+        System.out.println("Introduce una cadena de texto: ");
+        String cadena = scanner.next();
 //Ver si el string es palindromo.
 
 //ver cuantas mayusculas tiene la cadena.
+        int cantidadMayusculas= 0;
+        for (int i = 0;i<cadena.length();++i){
+    if (Character.isUpperCase(cadena.charAt(i))){
+        cantidadMayusculas++;
+    }
+}
 
+        System.out.println("La cadena de texto "+cadena+" tiene un total de "+cantidadMayusculas+" mayusculas.");
 //Si no es primo, factorizarlo.
 
 //Mostrar el tamaño de la cadena.
